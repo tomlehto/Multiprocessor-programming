a90aeabba0a56e1ded658c7bf85ef72c366da590
Julius Rintamäki <julius.rintamaki@iki.fi>
2020-02-11 20:12:27 +0200

    Add base for image_proc.c

---
+++ image_proc/image_proc.c
@@ -0,0 +1,12 @@
+/* Tomi Lehto 2508005, Julius Rintamäki 2507255 */
+
+#include "../LodePNG/lodepng.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+        
